06b16af5744ae6d2bef9a53f1a1afc8636cec358
Arpita21-p <arpita.a-potadar@capgemini.com>
2025-04-23 14:57:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './styles/Navbar.css';
-// const Navbar = () => {
-//  return (
-// <nav className="navbar">
-// <div className="nav-left">
-// <Link to="/">Home</Link>
-// <Link to="/members">Members</Link>
-// <Link to="/groups">Groups</Link>
-// <Link to="/photos">Photos</Link>
-// <Link to="/profile">Profile</Link>
-// </div>
-// <div className="nav-right">
-// <Link to="/signup">Sign Up</Link>
-// <Link to="/login">Login</Link>
-// </div>
-// </nav>
-//  );
-// };
-// export default Navbar;
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles/Navbar.css';
